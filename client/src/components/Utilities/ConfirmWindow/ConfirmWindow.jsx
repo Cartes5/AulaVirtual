a242b3cba1524f2c9bf04e7a229f429e665bfa54
Abraham Cartes <cartes5ironman@gmail.com>
2022-09-19 16:44:27 +0200

    Creacion Secciones

---
+++ client/src/components/Utilities/ConfirmWindow/ConfirmWindow.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import Button from "../Button/Button";
+import classes from "./ConfirmWindow.module.css";
+import { wrapperActions } from "../../../features/wrapper";
+import { currentUserActions } from "../../../features/currentUser";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { currentClassActions } from "../../../features/currentClass";
+import { isLoadingActions } from "../../../features/loading";
+import { errorActions } from "../../../features/error";
+
+const ConfirmWindow = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.CurrentUser.user);
+  const currentClass = useSelector((state) => state.CurrentClass.class);
+  const dark = useSelector((state) => state.DarkMode.isDarkMode);
+  console.log(currentClass);
+
+  let url;
+  user.role === "Mentor"
+    ? (url = "/api/teacher/classes/delete")
+    : (url = "/api/student/classes/delete");
+
+  const dispatchBunch = () => {
+    dispatch(currentUserActions.removeClass(currentClass));
+    dispatch(wrapperActions.setInitial());
+    dispatch(currentClassActions.setCurrentClass({ className: "" }));
+  };
+
+  const dropHandler = async () => {
+    console.log(currentClass);
+
+    dispatch(isLoadingActions.setIsLoading(true));
+    await axios
+      .put(url, { classId: currentClass._id, userId: user._id })
+      .then((serverRes) => {
+        dispatchBunch();
+        dispatch(isLoadingActions.setIsLoading(false));
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          dispatch(errorActions.setIsError(true));
+          dispatch(errorActions.setMsg("La clase ya no existe"));
+          dispatchBunch();
+        } else {
+          dispatch(errorActions.setIsError(true));
+          dispatch(errorActions.setMsg("Error del servidor, por favor intentelo de nuevo."));
+        }
+      });
+  };
+
+  const deleteHandler = async () => {
+    dispatch(isLoadingActions.setIsLoading(true));
+
+    await axios
+      .delete(url, { data: { classId: currentClass._id } })
+      .then((serverRes) => {
+        dispatchBunch();
+        dispatch(isLoadingActions.setIsLoading(false));
+      })
+      .catch((err) => {
+        dispatch(errorActions.setIsError(true));
+        dispatch(errorActions.setMsg("Error del servidor, por favor intentelo de nuevo."));
+      });
+  };
+
+  const cancelHandler = () => {
+    dispatch(wrapperActions.setInitial());
+  };
+
+  return (
+    <div className={classes.popUp}>
+      <p className={dark ? `${classes.darkP}` : `${classes.lightP}`}>
+        {user.role === "Mentor"
+          ? `borrar ${currentClass.className}?`
+          : `Abandonar
+           ${currentClass.className}?`}
+      </p>
+
+      <p className={classes.p}>Are your sure?</p>
+
+      <div className={classes.btnBox}>
+        <Button innerTxt={"Cancelar"} clickMe={cancelHandler} />
+        {user.role === "Mentor" ? (
+          <Button innerTxt={"Confirmar"} clickMe={deleteHandler} />
+        ) : (
+          <Button innerTxt={"Confirmar"} clickMe={dropHandler} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmWindow;
