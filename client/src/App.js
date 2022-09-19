cc48b79b29f2d1ac1a5eec3f60560aa146566845
Abraham Cartes <cartes5ironman@gmail.com>
2022-09-19 12:37:17 +0200

    Estructura Login y registro

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import './App.module.css';
+import classes from "./App.module.css";
+import { useSelector } from "react-redux";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Modal from "./modals/Modal/Modal";
+import io from "socket.io-client";
+
+const socket = io.connect("/");
 
 function App() {
+  const dark = useSelector((state) => state.DarkMode.isDarkMode);
+  const toggleLogReg = useSelector((state) => state.LogRegModal.isModal);
+
   return (
-    <div>
-      Hola Mundo
+    <div className={dark ? `${classes.darkApp}` : `${classes.lightApp}`}>
+      <Header />
+      {toggleLogReg && <Modal dark={dark} socket={socket} />}
+      <Hero />
+      <Footer />
     </div>
   );
 }
